d0b31fdaa285c9f9ec52020ba2c999e8026a1c84
Sergio Oseko <osk.sergio@gmail.com>
2022-10-15 10:58:57 -0300

    refactor: limit the values of hash fields (max. 11)

--- app/services/hashes_to_string.rb
+++ app/services/hashes_to_string.rb
@@ -10,7 +10,7 @@ class HashesToString
     result_string = ''
     @hashes.each do |i|
       result_string = "#{result_string} \n" unless result_string.empty?
-      result_string = "#{result_string}#{i[:name]}#{i[:cpf]}#{i[:state]}#{i[:value]}"
+      result_string = "#{result_string}#{i[:name][0..10].ljust(11)}#{i[:cpf][0..10].ljust(11)}#{i[:state][0..10].ljust(11)}#{i[:value][0..10].ljust(11)}"
     end
     result_string
   end
