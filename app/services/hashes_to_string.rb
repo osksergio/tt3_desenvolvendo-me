f7abef1f2a61e43aa7f2ce47b9f8d4e88f9cf98c
Sergio Oseko <osk.sergio@gmail.com>
2022-10-18 20:26:01 -0300

    feat: loading parameters from a yaml file

--- app/services/hashes_to_string.rb
+++ app/services/hashes_to_string.rb
@@ -1,55 +1,48 @@
 # frozen_string_literal: true
+require 'yaml'
 
 # Converter lista de hashes em string
 class HashesToString
   def initialize(arg)
     @hashes = arg
+    @config = File.read('format.yaml')
+    @app_config = YAML.load(@config)
+    #puts @app_config['cpf']['length']
   end
 
-  fields_config = File.read("#{Rails.root}/format.yaml")
-  HASHES_CONFIG = YAML.load(fields_config)
-
-  def hash_string
+  def hashes_to_string
     result_string = ''
+
     @hashes.each do |i|
       f_name  = i[:name]
       f_cpf   = i[:cpf]
       f_state = i[:state]
       f_value = i[:value]
 
-      f_name  = f_name[1..HASHES_CONFIG[Rails.env]['name']['length']]
-      f_cpf   = f_cpf[1..HASHES_CONFIG[Rails.env]['cpf']['length']]
-      f_state = f_state[HASHES_CONFIG[Rails.env]['state']['length']]
-      f_value = f_value[HASHES_CONFIG[Rails.env]['value']['length']]
-
-      if [HASHES_CONFIG[Rails.env]['name']['padding']] == 'zeroes'
-        char_name = '0'
-      else
-        char_name = ''
-      end
-
-      if [HASHES_CONFIG[Rails.env]['cpf']['padding']] == 'zeroes'
-        char_cpf = '0'
-      else
-        char_cpf = ''
-      end
-
-      if [HASHES_CONFIG[Rails.env]['state']['padding']] == 'zeroes'
-        char_state = '0'
-      else
-        char_state = ''
-      end
-
-      if [HASHES_CONFIG[Rails.env]['value']['padding']] == 'zeroes'
-        char_value = '0'
-      else
-        char_value = ''
-      end
-
+      f_name  = f_name[0..@app_config['name']['length']]
+      f_cpf   = f_cpf[0..@app_config['cpf']['length']]
+      f_state = f_state[0..@app_config['state']['length']]
+      f_value = f_value[0..@app_config['value']['length']]
 
       result_string = "#{result_string} \n" unless result_string.empty?
-      result_string = "#{result_string}#{i[:name][0..10].ljust(11)}#{i[:cpf][0..10].ljust(11)}#{i[:state][0..10].ljust(11)}#{i[:value][0..10].ljust(11)}"
+      result_string = "#{result_string}#{f_name}#{f_cpf}#{f_state}#{f_value}"
     end
     result_string
   end
 end
+
+hashes1 =
+  [
+    { name: 'Maria Neusa de Aparecida',
+      cpf: '97905796671',
+      state: 'Sao Paulo',
+      value: '1234' },
+    { name: 'Ricardo Fontes',
+      cpf: '44010762900',
+      state: 'Rio Grande do Sul',
+      value: '567' }
+  ]
+
+test1 = HashesToString.new(hashes1)
+result1 = test1.hashes_to_string
+puts result1
