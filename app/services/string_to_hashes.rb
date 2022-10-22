93d4bb52da6c5e05b622f94c1921133e8b751f75
Sergio Oseko <osk.sergio@gmail.com>
2022-10-22 13:03:44 -0300

    feat: string to hashes (incomplete)

--- app/services/string_to_hashes.rb
+++ app/services/string_to_hashes.rb
@@ -5,50 +5,48 @@ require 'yaml'
 class StringToHashes
   def initialize(str)
     @str = str
-    @config = File.read('layout_hashes.yaml')
+    @config = File.read('format.yaml')
     @app_config = YAML.load(@config)
   end
 
   def string_to_hashes
-    line_hash = []
-
+    array_hashes = []
     name = ''
     cpf = ''
     state = ''
     value = ''
+    line_position = 0
 
     # varredura da string
-    for i in 1..@str.length do
+    for i in 0..@str.length do
 
-      if @str[i] = '\n'
-        line_hash = ''
+      if (@str[i] == "\n")
+        array_hashes.push({name: name, cpf: cpf, state: state, value: value})
         name = ''
         cpf = ''
         state = ''
         value = ''
+        line_position = 0
+        i += 1
       end
 
-      for name_i in 1..@app_config['name']['length']
-        name = name + @str[i]
-      end
+      name += @str[i] if line_position <= @app_config['name']['length']
 
-      for cpf_i in 1..@app_config['cpf']['length']
-        cpf = cpf + @str[i]
-      end
+      cpf += @str[i] if line_position > (@app_config['name']['length']) and line_position <= (@app_config['name']['length'] + @app_config['cpf']['length'])
 
-      for state_i in 1..@app_config['state']['length']
-        state = state + @str[i]
-      end
+      state += @str[i] if line_position > (@app_config['name']['length'] + @app_config['cpf']['length']) and
+        line_position <= (@app_config['name']['length'] + @app_config['cpf']['length'] + @app_config['state']['length'])
 
-      for value_i in 1..@app_config['value']['length']
-        value = value + @str[i]
-      end
+      value += @str[i] if line_position > (@app_config['name']['length'] + @app_config['cpf']['length'] + @app_config['state']['length']) and
+        line_position <= (@app_config['value']['length'])
+
+      line_position += 1
     end
-    line_hash.push({name: name, cpf: cpf, state: state, value: value})
+
+    puts array_hashes
   end
 end
 
 test1 = StringToHashes.new("Maria Neusa de 97905796671Sao Paulo     00001234 \nRicardo Fontes44010762900Rio Grande do S00000567")
 result1 = test1.string_to_hashes
-
 puts result1
